8f7c262650649d9b9c517c640d61ccc7b53cfa93
demidovichpavel <demidovicpav4@gmail.com>
2023-02-20 01:07:24 +0300

    this work

--- backend/settings.py
+++ backend/settings.py
@@ -132,8 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'build/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'build/static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'build/static/',
+    '/build/static/'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
