912ddf8aad21957e12a60f472d2f1e9411665e1e
demidovichpavel <demidovicpav4@gmail.com>
2023-02-22 00:57:51 +0300

    this work

--- aviso_api/models.py
+++ aviso_api/models.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 
 class News(models.Model):
     title = models.CharField('Заголовок новости', max_length=150)
+    # img = models.ImageField('Фотография новости', default='default.png')
     content = models.TextField('Текст новости')
     time_created = models.DateTimeField('Дата создания', auto_now_add=True)
     is_published = models.BooleanField('Публично ли', default=True)
@@ -36,11 +37,11 @@ class Articles(models.Model):
 class Order(models.Model):
     title = models.CharField('Название заказа', max_length=150)
     content = models.TextField('Текс заказа')
-    time_created = models.DateTimeField('Время создания', auto_now_add=True)
+    time_created = models.DateTimeField('Дата создания', auto_now_add=True)
     is_published = models.BooleanField('Публично ли', default=True)
     
     creator = models.OneToOneField(User, on_delete=models.SET_NULL, null=True, related_name='creator')
-    worker = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='worker')
+    worker = models.ManyToManyField(User, null=True, related_name='worker')
     
     def __str__(self):
         return self.title
--- aviso_api/serializers.py
+++ aviso_api/serializers.py
@@ -4,6 +4,7 @@ from rest_framework.parsers import JSONParser
 from rest_framework.renderers import JSONRenderer
 from .models import *
 
+
 class NewsSerializer(serializers.ModelSerializer):
     class Meta:
         model = News
